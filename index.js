830b5cebba2dd21c1cb301bc6b003e4c4b859986
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-03-30 11:35:24 +0200

    added addeventListener

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ let bm = document.getElementById('b-minutes');
 let bs = document.getElementById('b-seconds');
 
 //store a reference to a timer varaiable
+let startTimer; //if we don`t give it a value is automatically undefined
 
-let startTimer;
+//2 step after setting the function timer we setting the addEventListener
+start.addEventListener('click', function() {
+	if (startTimer === undefined) {
+		startTimer = setInterval(timer, 1000);
+	} else {
+		alert('Timer is already running');
+	}
+});
 
+reset.addEventListener('click', function() {
+	wm.innerText = 25;
+	ws.innerText = '00';
+
+	bm.innerText = 5;
+	bs.innerText = '00';
+	document.getElementById('counter').innerText = 0;
+	stopInterval();
+	startTimer = undefined;
+});
+
+stop.addEventListener('click', function() {
+	stopInterval();
+	startTimer = undefined;
+});
+
+//1 step setting the start function Timer
 function timer() {
 	//Work Timer CountDown
 	if (ws.innerText != 0) {
@@ -29,4 +54,19 @@ function timer() {
 			bm.innerText--;
 		}
 	}
+	//Increment Counter by one if one full cycle is completed
+	if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0) {
+		wm.innerText = 25;
+		ws.innerText = '00';
+
+		bm.innerText = 5;
+		bs.innerText = '00';
+
+		document.getElementById('counter').innerText++;
+	}
+}
+
+//3 step setting the stop timer function
+function stopInterval() {
+	clearInterval(startTimer);
 }
